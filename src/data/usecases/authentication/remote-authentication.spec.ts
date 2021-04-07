8dba247eb2127b59cb3e880b86ff56a8fd3f64c4
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-04-07 18:56:23 -0300

    refactor: adding path alias for the project

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,8 @@
-import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { HttpPostClientSpy } from "@/data/test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
+import { mockAuthentication } from "@/domain/test/mock-authentication";
+
 import { internet } from 'faker';
-import { mockAuthentication } from "../../../domain/test/mock-authentication";
 
 type SutTypes = {
     sut: RemoteAuthentication;
--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,4 +1,4 @@
-import { AuthenticationParams } from '../usecases/authentication';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { internet } from 'faker';
 
 export const mockAuthentication = (): AuthenticationParams => ({
