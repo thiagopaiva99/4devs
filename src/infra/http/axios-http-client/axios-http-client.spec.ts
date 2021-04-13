e53f4b27728ba841f05c00a03d33f92f8795cebe
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-04-13 20:09:18 -0300

    refactor: move axios creation to a factory method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,11 +5,15 @@ import { internet } from 'faker';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const axiosFactory = (): AxiosHttpClient => {
+    return new AxiosHttpClient();
+}
+
 describe('AxiosHttpClient', () => {
-    test('should call axios with correct URL', async () => {
+    test('should call axios with correct URL and verb', async () => {
         const url = internet.url();
-        const axiosClient = new AxiosHttpClient();
+        const axiosClient = axiosFactory();
         await axiosClient.post({ url });
-        expect(mockedAxios).toHaveBeenCalledWith(url);
+        expect(mockedAxios.post).toHaveBeenCalledWith(url);
     })
 })
