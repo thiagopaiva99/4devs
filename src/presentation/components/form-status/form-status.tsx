02a0d08e1353f97daa15868bdedc510e968078c1
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-08-24 20:07:33 -0300

    refactor: creating a globals style file

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,12 @@
 import React, { memo } from 'react';
 import Spinner from '../spinner/spinner';
-import Styles from './input-styles.scss';
+import Styles from './form-status-styles.scss';
 
 const FormStatus: React.FC = () => (
     <section className={Styles.errorWrapper}>
-                    <Spinner className={Styles.spinner} />
-                    <span className={Styles.error}>Erro</span>
-                </section>
+        <Spinner className={Styles.spinner} />
+        <span className={Styles.error}>Erro</span>
+    </section>
 );
 
 export default FormStatus;
