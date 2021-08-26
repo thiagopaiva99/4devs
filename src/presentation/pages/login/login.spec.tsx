44b136f41de320863051f4e76206326b026449e9
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-08-26 20:16:16 -0300

    refactor: change validationStub to initialize without error

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -8,18 +8,20 @@ import faker from 'faker';
 
 type LoginComponentFactoryTypes = {
     component: RenderResult;
-    validationStub: ValidationStub;
 }
 
-const loginComponentFactory = (): LoginComponentFactoryTypes  => {
-    const validationStub = new ValidationStub()
-    validationStub.errorMessage = faker.random.words();
+type FactoryParams = {
+    validationError: string;
+}
+
+const loginComponentFactory = (params?: FactoryParams): LoginComponentFactoryTypes  => {
+    const validationStub = new ValidationStub();
+    validationStub.errorMessage = params?.validationError;
 
     const component = render(<Login validation={validationStub} />);
 
     return {
-        component,
-        validationStub
+        component
     }
 }
 
@@ -27,7 +29,8 @@ describe('Login Component', () => {
     afterEach(cleanup);
 
     test('should start with initial state', () => {
-        const { component, validationStub } = loginComponentFactory();
+        const validationError = faker.random.words();
+        const { component } = loginComponentFactory({ validationError });
 
         const errorWrapper = component.getByTestId('error-wrapper');
         expect(errorWrapper.childElementCount).toBe(0);
@@ -36,37 +39,36 @@ describe('Login Component', () => {
         expect(submitButton.disabled).toBe(true);
 
         const emailStatus = component.getByTestId('email-status');
-        expect(emailStatus.title).toBe(validationStub.errorMessage);
+        expect(emailStatus.title).toBe(validationError);
         expect(emailStatus.textContent).toBe('🔴');
 
         const passwordStatus = component.getByTestId('email-status');
-        expect(passwordStatus.title).toBe(validationStub.errorMessage);
+        expect(passwordStatus.title).toBe(validationError);
         expect(passwordStatus.textContent).toBe('🔴');
     });
 
     test('should show email error if validation fails', () => {
-        const { component, validationStub } = loginComponentFactory();
-             
+        const validationError = faker.random.words();
+        const { component } = loginComponentFactory({ validationError });
         const emailInput = component.getByTestId('email-field');
         fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
         const emailStatus = component.getByTestId('email-status');
-        expect(emailStatus.title).toBe(validationStub.errorMessage);
+        expect(emailStatus.title).toBe(validationError);
         expect(emailStatus.textContent).toBe('🔴');
     });
 
     test('should show password error if validation fails', () => {
-        const { component, validationStub } = loginComponentFactory();
-             
+        const validationError = faker.random.words();
+        const { component } = loginComponentFactory({ validationError });
         const passwordInput = component.getByTestId('password-field');
         fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
         const passwordStatus = component.getByTestId('password-status');
-        expect(passwordStatus.title).toBe(validationStub.errorMessage);
+        expect(passwordStatus.title).toBe(validationError);
         expect(passwordStatus.textContent).toBe('🔴');
     });
 
     test('should show valid email state if validation succeeds', () => {
-        const { component, validationStub } = loginComponentFactory();
-        validationStub.errorMessage = null;
+        const { component } = loginComponentFactory();
         const emailInput = component.getByTestId('email-field');
         fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
         const emailStatus = component.getByTestId('email-status');
@@ -75,8 +77,7 @@ describe('Login Component', () => {
     });
 
     test('should show valid password state if validation succeeds', () => {
-        const { component, validationStub } = loginComponentFactory();
-        validationStub.errorMessage = null;
+        const { component } = loginComponentFactory();
         const passwordInput = component.getByTestId('password-field');
         fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
         const passwordStatus = component.getByTestId('password-status');
@@ -85,8 +86,7 @@ describe('Login Component', () => {
     });
 
     test('should enable submit button if form is valid', () => {
-        const { component, validationStub } = loginComponentFactory();
-        validationStub.errorMessage = null;
+        const { component } = loginComponentFactory();
         const emailInput = component.getByTestId('email-field');
         fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
         const passwordInput = component.getByTestId('password-field');
