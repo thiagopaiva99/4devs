fd312e4d2c4525c4ad65e2e2d6b9f59c194f44c1
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-08-26 19:23:34 -0300

    test: ensure that email is being validated on input data

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 
-import { render, RenderResult } from '@testing-library/react';
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react';
 import { Login } from './login';
+import { Validation } from '@/presentation/protocols/validation';
 
 type LoginComponentFactoryTypes = {
     component: RenderResult;
+    validationSpy: ValidationSpy;
+}
+
+class ValidationSpy implements Validation {
+    errorMessage: string;
+    input: object;
+
+    validate(input: object): string {
+        this.input = input;
+        return this.errorMessage;
+    }
+
 }
 
 const loginComponentFactory = (): LoginComponentFactoryTypes  => {
-    const component = render(<Login />);
+    const validationSpy = new ValidationSpy()
+    const component = render(<Login validation={validationSpy} />);
 
     return {
-        component
+        component,
+        validationSpy
     }
 }
 
 describe('Login Component', () => {
+    afterEach(cleanup);
+
     test('should start with initial state', () => {
         const { component } = loginComponentFactory();
 
@@ -32,5 +49,15 @@ describe('Login Component', () => {
         const passwordStatus = component.getByTestId('email-status');
         expect(passwordStatus.title).toBe('Campo obrigatório');
         expect(passwordStatus.textContent).toBe('🔴');
+    });
+
+    test('should call Validation with correct value', () => {
+        const { component, validationSpy } = loginComponentFactory();
+
+        const emailInput = component.getByTestId('email-field')
+        fireEvent.input(emailInput, { target: { value: 'any_email' } })
+        expect(validationSpy.input).toEqual({
+            email: 'any_email'
+        })
     });
 });
