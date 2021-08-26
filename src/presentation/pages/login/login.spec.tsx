b4d6f30b81ba1822a5966ea462d47a87ff362959
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-08-26 19:01:59 -0300

    refactor: creating a factory for login component

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import { Login } from './login';
+
+type LoginComponentFactoryTypes = {
+    component: RenderResult;
+}
+
+const loginComponentFactory = (): LoginComponentFactoryTypes  => {
+    const component = render(<Login />);
+
+    return {
+        component
+    }
+}
 
 describe('Login Component', () => {
     test('should start with initial state', () => {
-        const { getByTestId } = render(<Login />);
+        const { component } = loginComponentFactory();
 
-        const errorWrapper = getByTestId('error-wrapper');
+        const errorWrapper = component.getByTestId('error-wrapper');
         expect(errorWrapper.childElementCount).toBe(0);
 
-        const submitButton = getByTestId('submit') as HTMLButtonElement;
+        const submitButton = component.getByTestId('submit') as HTMLButtonElement;
         expect(submitButton.disabled).toBe(true);
 
-        const emailStatus = getByTestId('email-status');
+        const emailStatus = component.getByTestId('email-status');
         expect(emailStatus.title).toBe('Campo obrigatório');
         expect(emailStatus.textContent).toBe('🔴');
 
-        const passwordStatus = getByTestId('email-status');
+        const passwordStatus = component.getByTestId('email-status');
         expect(passwordStatus.title).toBe('Campo obrigatório');
         expect(passwordStatus.textContent).toBe('🔴');
     });
