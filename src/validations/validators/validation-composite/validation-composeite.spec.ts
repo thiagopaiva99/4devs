8f90737efbbfe394852bbb02b474f74de583ef53
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 20:27:12 -0300

    test: ensure validation composite returns the first error found

--- src/validations/validators/validation-composite/validation-composeite.spec.ts
+++ src/validations/validators/validation-composite/validation-composeite.spec.ts
@@ -5,9 +5,10 @@ describe('ValidationComposite', () => {
     test('should return error if any validation fails', () => {
         const fieldValidationSpy = new FieldValidationSpy('any_field');
         const fieldValidationSpy2 = new FieldValidationSpy('any_field');
-        fieldValidationSpy2.error = new Error('any_error_message');
+        fieldValidationSpy.error = new Error('first_error');
+        fieldValidationSpy2.error = new Error('second_error');
         const validation = new ValidationComposite([ fieldValidationSpy, fieldValidationSpy2 ]);
         const error = validation.validate('any_field', 'any_value');
-        expect(error).toBe('any_error_message')
+        expect(error).toBe('first_error')
     })
 })
