f8c35e63aaff5426aab24da8be3a75c29aa396e9
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 20:31:16 -0300

    refactor: move validation creation to a factory method

--- src/validations/validators/validation-composite/validation-composeite.spec.ts
+++ src/validations/validators/validation-composite/validation-composeite.spec.ts
@@ -1,13 +1,29 @@
 import { ValidationComposite } from "./validation-composite";
 import { FieldValidationSpy } from "@/validations/validators/test/mock-field-validation";
 
+type ValidationFactoryTypes = {
+    validation: ValidationComposite,
+    fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const validationFactory = (): ValidationFactoryTypes => {
+    const fieldValidationsSpy = [
+        new FieldValidationSpy('any_field'), 
+        new FieldValidationSpy('any_field')
+    ]
+    const validation = new ValidationComposite(fieldValidationsSpy);
+
+    return { 
+        validation, 
+        fieldValidationsSpy
+    }
+}
+
 describe('ValidationComposite', () => {
     test('should return error if any validation fails', () => {
-        const fieldValidationSpy = new FieldValidationSpy('any_field');
-        const fieldValidationSpy2 = new FieldValidationSpy('any_field');
-        fieldValidationSpy.error = new Error('first_error');
-        fieldValidationSpy2.error = new Error('second_error');
-        const validation = new ValidationComposite([ fieldValidationSpy, fieldValidationSpy2 ]);
+        const { validation, fieldValidationsSpy } = validationFactory();    
+        fieldValidationsSpy[0].error = new Error('first_error');
+        fieldValidationsSpy[1].error = new Error('second_error');
         const error = validation.validate('any_field', 'any_value');
         expect(error).toBe('first_error')
     })
