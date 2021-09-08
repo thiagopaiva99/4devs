f70c8d5d3f9583cc268f1cc1a106e1a3d607810a
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 19:48:20 -0300

    test: ensure email validation returns falsy if email is valid

--- src/validations/validators/email/email.validation.spec.ts
+++ src/validations/validators/email/email.validation.spec.ts
@@ -5,8 +5,14 @@ import faker from 'faker';
 
 describe('EmailValidation', () => {
     test('should return error if email is invalid', () => {
-        const validator = new EmailValidation('email');
-        const error = validator.validate('');
+        const validator = new EmailValidation(faker.database.column());
+        const error = validator.validate(faker.random.word());
         expect(error).toEqual(new InvalidFieldError());
+    })
+
+    test('should return falsy if email is valid', () => {
+        const validator = new EmailValidation(faker.database.column());
+        const error = validator.validate(faker.internet.email());
+        expect(error).toBeFalsy();
     })
 })
