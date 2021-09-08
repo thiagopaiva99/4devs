0e3fb2b606eef10f127906567452fce72876ebae
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 19:26:56 -0300

    refactor: move validation create to a factory method

--- src/validations/required-field/required-field.validation.spec.ts
+++ src/validations/required-field/required-field.validation.spec.ts
@@ -3,15 +3,17 @@ import { RequiredFieldValidation } from "./required-field.validation";
 
 import faker from 'faker';
 
+const validationFactory = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column());
+
 describe('RequiredFieldValidation', () => {
     test('should return error if field is empty', () => {
-        const validation = new RequiredFieldValidation('email');
+        const validation = validationFactory();
         const error = validation.validate('');
         expect(error).toEqual(new RequiredFieldError())
     })
 
     test('should return falsy if field is not empty', () => {
-        const validation = new RequiredFieldValidation('email');
+        const validation = validationFactory();
         const error = validation.validate(faker.random.word());
         expect(error).toBeFalsy()
     })
