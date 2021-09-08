471920f47dc5b0bd062a15c768c96181cf3f8afc
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 19:49:51 -0300

    refactor: creating factory to return email validator

--- src/validations/validators/email/email.validation.spec.ts
+++ src/validations/validators/email/email.validation.spec.ts
@@ -3,15 +3,17 @@ import { InvalidFieldError } from '@/validations/errors';
 
 import faker from 'faker';
 
+const validationFactory = (): EmailValidation => new EmailValidation(faker.database.column());
+
 describe('EmailValidation', () => {
     test('should return error if email is invalid', () => {
-        const validator = new EmailValidation(faker.database.column());
+        const validator = validationFactory();
         const error = validator.validate(faker.random.word());
         expect(error).toEqual(new InvalidFieldError());
     })
 
     test('should return falsy if email is valid', () => {
-        const validator = new EmailValidation(faker.database.column());
+        const validator = validationFactory();
         const error = validator.validate(faker.internet.email());
         expect(error).toBeFalsy();
     })
