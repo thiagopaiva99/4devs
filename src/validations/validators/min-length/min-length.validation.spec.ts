b9c80e7d6e4667c45721c4512b4fada71387d7b0
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-08 20:06:13 -0300

    refactor: creating factory to return min length validator

--- src/validations/validators/min-length/min-length.validation.spec.ts
+++ src/validations/validators/min-length/min-length.validation.spec.ts
@@ -1,16 +1,22 @@
 import { InvalidFieldError } from "@/validations/errors";
 import { MinLengthValidation } from "./min-length.validation";
 
+import faker from 'faker';
+
+const validationFactory = (minLength: number): MinLengthValidation => new MinLengthValidation(faker.database.column(), minLength);
+
 describe('MinLengthValidation', () => {
     test('should return error if value is invalid', () => {
-        const validator = new MinLengthValidation('field', 5);
-        const error = validator.validate('four');
+        const minLength = faker.datatype.number();
+        const validator = validationFactory(minLength)
+        const error = validator.validate(faker.random.alphaNumeric(minLength - 1));
         expect(error).toEqual(new InvalidFieldError());
     })
 
     test('should return falsy if value is valid', () => {
-        const validator = new MinLengthValidation('field', 5);
-        const error = validator.validate('cinco');
+        const minLength = faker.datatype.number();
+        const validator = validationFactory(minLength)
+        const error = validator.validate(faker.random.alphaNumeric(minLength + 1));
         expect(error).toBeFalsy()
     })
 })
