46514a8e87981bab3984444260ada3d6b6a0c172
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-09 12:35:29 -0300

    test: ensure validation vbuilder returns a list with more than one validation

--- src/validations/validators/builder/validation-builder.spec.ts
+++ src/validations/validators/builder/validation-builder.spec.ts
@@ -15,4 +15,13 @@ describe('ValidationBuilder', () => {
         const validations = ValidationBuilder.field('any_field').email().build();
         expect(validations).toEqual([new EmailValidation('any_field')])
     })
+
+    test('should return more than one validation', () => {
+        const validations = ValidationBuilder.field('any_field').email().required().min(3).build();
+        expect(validations).toEqual([
+            new EmailValidation('any_field'), 
+            new RequiredFieldValidation('any_field'),
+            new MinLengthValidation('any_field', 3)
+        ])
+    })
 })
