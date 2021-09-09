b2f0422492eea468e983dc697ff2df3642ba3f2d
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-09 12:32:37 -0300

    test: ensure validation builder returns a min length validation

--- src/validations/validators/builder/validation-builder.spec.ts
+++ src/validations/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,13 @@
-import { RequiredFieldValidation, ValidationBuilder } from "@/validations/validators";
-
-
+import { RequiredFieldValidation, ValidationBuilder, MinLengthValidation } from "@/validations/validators";
 
 describe('ValidationBuilder', () => {
     test('should return RequiredFieldValidation', () => {
         const validations = ValidationBuilder.field('any_field').required().build();
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    })
+
+    test('should return MinLengthValidation', () => {
+        const validations = ValidationBuilder.field('any_field').min(5).build();
+        expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
     })
 })
