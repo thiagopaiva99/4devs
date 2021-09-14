f40795cfa1e2f4584184cd7e311f3700f849416b
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-14 19:59:44 -0300

    refactor: make factory for local storage adapter

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -1,11 +1,21 @@
 import faker from 'faker'
 import { LocalStorageAdapter } from './local-storage-adapter'
 
+const localStorageAdapterFactory = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
   beforeEach(localStorage.clear)
 
   test('should call localStorage with correct values', async () => {
-    const localStorageAdapter = new LocalStorageAdapter()
+    const localStorageAdapter = localStorageAdapterFactory()
+    const key = faker.database.column()
+    const value = faker.random.word()
+    await localStorageAdapter.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
+  })
+
+  test('should throws if localStorage throws', async () => {
+    const localStorageAdapter = localStorageAdapterFactory()
     const key = faker.database.column()
     const value = faker.random.word()
     await localStorageAdapter.set(key, value)
