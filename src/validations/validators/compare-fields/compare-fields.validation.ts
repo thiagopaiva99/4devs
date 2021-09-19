be33f931607c89b23804af926a137a280ba2fc65
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-19 16:27:19 -0300

    feat: ensure compare fields validation returns falsy if validator succeed

--- src/validations/validators/compare-fields/compare-fields.validation.ts
+++ src/validations/validators/compare-fields/compare-fields.validation.ts
@@ -5,6 +5,6 @@ export class CompareFieldsValidation implements FieldValidation {
   constructor (readonly field: string, private readonly valueToCompare: string) {}
 
   validate (value: string): Error {
-    return new InvalidFieldError()
+    return value !== this.valueToCompare ? new InvalidFieldError() : null
   }
 }
