8bf59f09de6ff253b1f3a5d3f487e1b6a0e775e0
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-19 17:33:24 -0300

    feat: ensure signup change name status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,36 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './signup-styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const Signup: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+
+const Signup: React.FC<Props> = ({ validation }) => {
+  const [state, setState] = useState({
     isLoading: false,
-    nameError: 'Campo obrigatório',
+    name: '',
+    nameError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
   })
 
-  return (
-        <div className={Styles.signup}>
-            <LoginHeader />
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', state.name)
+    })
+  }, [state.name])
 
-            <Context.Provider value={{ state }}>
-                <form className={Styles.form}>
-                    <h2>Criar Conta</h2>
-                    <Input type="text" name="name" placeholder="Digite seu nome" />
-                    <Input type="email" name="email" placeholder="Digite seu email" />
-                    <Input type="password" name="password" placeholder="Digite sua senha" />
-                    <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha" />
-                    <button type="submit" data-testid="submit" disabled>Criar Conta</button>
-                    <span className={Styles.link}>Voltar para Login</span>
-                    <FormStatus />
-                </form>
-            </Context.Provider>
-            <Footer />
-        </div>
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+
+      <Context.Provider value={{ state, setState }}>
+        <form className={Styles.form}>
+          <h2>Criar Conta</h2>
+          <Input type="text" name="name" placeholder="Digite seu nome" />
+          <Input type="email" name="email" placeholder="Digite seu email" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha" />
+          <button type="submit" data-testid="submit" disabled>Criar Conta</button>
+          <span className={Styles.link}>Voltar para Login</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
+      <Footer />
+    </div>
   )
 }
 
