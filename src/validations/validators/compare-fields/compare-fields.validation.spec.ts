cbad715c543341b1fe34b3431f00c8a52a039916
thiagopaiva99 <thiagopaiva99@gmail.com>
2021-09-19 16:25:48 -0300

    test: ensure compare fields validation returns error if valid fails

---
+++ src/validations/validators/compare-fields/compare-fields.validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidFieldError } from '@/validations/errors'
+
+import { database, random } from 'faker'
+import { CompareFieldsValidation } from './compare-fields.validation'
+
+const validationFactory = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(database.column(), valueToCompare)
+
+describe('CompareFieldsValidation', () => {
+  test('should return error if compare is invalid', () => {
+    const validation = validationFactory(random.word())
+    const error = validation.validate(random.word())
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
