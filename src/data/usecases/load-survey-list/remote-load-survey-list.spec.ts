680173df0b93ca5b263257a69dc39ae4ee25584d
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-09 20:08:22 -0300

    refactor: moving data creation for a factory method

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -2,11 +2,22 @@ import { HttpGetClientSpy } from '@/data/test'
 import { internet } from 'faker'
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
 
+type Types = {
+  remoteSurveyList: RemoteLoadSurveyList
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const surveyFactory = (url: string = internet.url()): Types => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const remoteSurveyList = new RemoteLoadSurveyList(url, httpGetClientSpy)
+
+  return { httpGetClientSpy, remoteSurveyList }
+}
+
 describe('RemoteLoadSurveyList', () => {
   test('should call HttpGetClient with correct url', async () => {
     const url = internet.url()
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const remoteSurveyList = new RemoteLoadSurveyList(url, httpGetClientSpy)
+    const { httpGetClientSpy, remoteSurveyList } = surveyFactory(url)
     await remoteSurveyList.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
   })
