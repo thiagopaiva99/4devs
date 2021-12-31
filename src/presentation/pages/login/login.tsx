30f24f34a930bd05eb0e34d85bc57d14feba7203
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-31 11:07:27 -0300

    refactor: adjusting container class pattern

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -70,7 +70,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
   }
 
   return (
-    <div className={Styles.login}>
+    <div className={Styles.loginWrap}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
         <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
