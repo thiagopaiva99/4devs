218d2d9363c86161eeefb611a2d1efe48e0056e5
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-11 22:12:33 -0300

    refactor: adjusting components usage on surveys list

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,27 +1,17 @@
-import { Footer, Logo } from '@/presentation/components'
+import { Footer, Header, Icon, IconName } from '@/presentation/components'
 import React from 'react'
-import { Icon, IconName } from './components/Icon'
 import Styles from './survey-list.styles.scss'
 
 const SurveyList: React.FC = () => {
   return (
         <div className={Styles.surveyListWrap}>
-            <header className={Styles.headerWrap}>
-                <div className={Styles.headerContent}>
-                    <Logo />
-                    <div className={Styles.userInfoWrap}>
-                        <span>Thiago</span>
-                        <a href="#">Sair</a>
-                    </div>
-                </div>
-            </header>
-
+            <Header />
             <div className={Styles.contentWrap}>
                 <h2>Enquetes</h2>
                 <ul>
                     <li>
                         <div className={Styles.surveyContent}>
-                            <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+                            <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
                             <time>
                                 <span className={Styles.day}>31</span>
                                 <span className={Styles.month}>12</span>
