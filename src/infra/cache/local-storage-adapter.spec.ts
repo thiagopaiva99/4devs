6c71bd27f641cc018da5d5a51a5136a3ee6bd48c
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-14 18:47:30 -0300

    test: ensure local storage adapter calls localstorage getItem with correct values

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -7,11 +7,20 @@ const localStorageAdapterFactory = (): LocalStorageAdapter => new LocalStorageAd
 describe('LocalStorageAdapter', () => {
   beforeEach(localStorage.clear)
 
-  test('should call localStorage with correct values', () => {
+  test('should call localStorage.setItem with correct values', () => {
     const localStorageAdapter = localStorageAdapterFactory()
     const key = faker.database.column()
     const value = random.objectElement<AccountModel>()
     localStorageAdapter.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+
+  test('should call localStorage.getItem with correct values', () => {
+    const localStorageAdapter = localStorageAdapterFactory()
+    const key = faker.database.column()
+    const value = random.objectElement<AccountModel>()
+    const getSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+    expect(localStorageAdapter.get(key)).toEqual(value)
+    expect(getSpy).toHaveBeenCalledWith(key)
   })
 })
