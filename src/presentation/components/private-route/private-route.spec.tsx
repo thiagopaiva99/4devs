92c6fca2429be142674fc83e53c636befe577c7c
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-17 17:14:01 -0300

    refactor: creating the factory for common render test

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
+
+type PrivateRouteFactoryTypes = {
+  history: MemoryHistory
+}
+
+const privateRouteFactory = (): PrivateRouteFactoryTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(
+<Router history={history}>
+        <PrivateRoute />
+
+    </Router>
+  )
+
+  return {
+    history
+  }
+}
 
 describe('PrivateRoute', () => {
   it('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    render(
-        <Router history={history}>
-            <PrivateRoute />
-
-        </Router>
-    )
+    const { history } = privateRouteFactory()
     expect(history.location.pathname).toBe('/login')
   })
 })
