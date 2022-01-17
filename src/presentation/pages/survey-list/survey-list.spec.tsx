49337dc262d7e7f93659082a5fdf9ccf2d833018
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-17 19:06:32 -0300

    test: ensure survey list component calls load survey list use case

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,9 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 import React from 'react'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
-const surveyListFactory = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SurveyListFactoryTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const surveyListFactory = (): SurveyListFactoryTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList Component', () => {
@@ -11,5 +29,10 @@ describe('SurveyList Component', () => {
     surveyListFactory()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  it('should call load survey list', () => {
+    const { loadSurveyListSpy } = surveyListFactory()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
