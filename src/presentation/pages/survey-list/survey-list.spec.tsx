0ca4983c4c1f4ca13b8c907bb4510c6c6fb38301
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-17 18:53:51 -0300

    refactor: moving componente rendering to inside a factory

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 import React from 'react'
 
+const surveyListFactory = (): void => {
+  render(<SurveyList />)
+}
+
 describe('SurveyList Component', () => {
   it('should present 4 empty items on start', () => {
-    render(<SurveyList />)
+    surveyListFactory()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
   })
