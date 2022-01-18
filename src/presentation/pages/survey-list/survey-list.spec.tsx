a62ce070c8358e5416db3a5606c9174fb04c6128
Thiago Galvani <thiagopaiva99@gmail.com>
2022-01-18 13:51:20 -0300

    test: ensure survey list render survey items on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,14 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 import React from 'react'
 import { LoadSurveyList } from '@/domain/usecases'
 import { SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
+  surveys = mockSurveyListModel()
   async loadAll (): Promise<SurveyModel[]> {
     this.callsCount++
-    return []
+    return this.surveys
   }
 }
 
@@ -25,14 +27,23 @@ const surveyListFactory = (): SurveyListFactoryTypes => {
 }
 
 describe('SurveyList Component', () => {
-  it('should present 4 empty items on start', () => {
+  it('should present 4 empty items on start', async () => {
     surveyListFactory()
     const surveyList = screen.getByTestId('survey-list')
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  it('should call load survey list', () => {
+  it('should call load survey list', async () => {
     const { loadSurveyListSpy } = surveyListFactory()
     expect(loadSurveyListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  it('should render survey items on success', async () => {
+    surveyListFactory()
+    const surveyList = screen.getByTestId('survey-list')
+    await waitFor(() => surveyList)
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3)
   })
 })
