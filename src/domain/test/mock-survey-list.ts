8143a21807177637aa7dc558d2eb1c8e39882412
Thiago Galvani <69972382+ThiagoGalvaniTR@users.noreply.github.com>
2022-01-18 11:57:54 -0300

    test: ensure survey item load survey data correctly

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -2,20 +2,24 @@ import { datatype, date, internet, random } from 'faker'
 import { SurveyModel } from '../models'
 
 export const mockSurveyListModel = (): SurveyModel[] => ([
-  {
-    id: datatype.uuid(),
-    question: random.words(10),
-    didAnswer: datatype.boolean(),
-    date: date.recent(),
-    answers: [
-      {
-        answer: random.words(50),
-        image: internet.url()
-      },
-      {
-        answer: random.words(50),
-        image: internet.url()
-      }
-    ]
-  }
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel()
 ])
+
+export const mockSurveyModel = (): SurveyModel => ({
+  id: datatype.uuid(),
+  question: random.words(10),
+  didAnswer: datatype.boolean(),
+  date: date.recent(),
+  answers: [
+    {
+      answer: random.words(50),
+      image: internet.url()
+    },
+    {
+      answer: random.words(50),
+      image: internet.url()
+    }
+  ]
+})
---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SurveyItem } from '@/presentation/pages/survey-list/components'
+import { mockSurveyModel } from '@/domain/test'
+import { IconName } from '@/presentation/components'
+
+describe('SurveyItem Component', () => {
+  it('should render with correct values', () => {
+    const survey = mockSurveyModel()
+    survey.didAnswer = true
+    survey.date = new Date('2022-01-18T00:00:00')
+    render(<SurveyItem survey={survey} />)
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('18')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2022')
+  })
+})
